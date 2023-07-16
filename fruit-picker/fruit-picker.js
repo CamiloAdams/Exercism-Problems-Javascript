fdcf7478bd949520d0767980fae5eb7d8d4f5583
Camilo Adames <camiloadames1108@gmail.com>
2023-07-15 21:31:22 -0500

    fruit-picker solution

---
+++ fruit-picker/fruit-picker.js
@@ -0,0 +1,49 @@
+/// <reference path="./global.d.ts" />
+//
+// @ts-check
+//
+// The lines above enable type checking for this file. Various IDEs interpret
+// the @ts-check and reference directives. Together, they give you helpful
+// autocompletion when implementing this exercise. You don't need to understand
+// them in order to use it.
+//
+// In your own projects, files, and code, you can play with @ts-check as well.
+
+import { notify } from "./notifier";
+import { order } from "./grocer";
+
+/**
+ * @return void
+ */
+export function onSuccess() {
+  // implement the onSuccess callback to call notify with a success message
+  return notify({ message: "SUCCESS" });
+}
+
+/**
+ * @return void
+ */
+export function onError() {
+  return notify({ message: "ERROR" });
+}
+
+/**
+ * @param {GrocerQuery} query
+ * @param {FruitPickerSuccessCallback} onSuccessCallback
+ * @param {FruitPickerErrorCallback} onErrorCallback
+ * @return void
+ */
+export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
+  return order(query, onSuccessCallback, onErrorCallback);
+  // implement the orderFromGrocer function to order the query
+}
+
+/**
+ * @param {string} variety
+ * @param {number} quantity
+ * @return void
+ */
+export function postOrder(variety, quantity) {
+  //implement the postOrder function to create a query and order
+  return orderFromGrocer({ variety, quantity }, onSuccess, onError);
+}
